a3aa5bd27995e48abe7d37083763314172cb820e
rizka30 <2041720095@student.polinema.ac.id>
2022-09-24 17:28:41 +0700

    WhiteBoxTesting

--- tests/Feature/MhsTest.php
+++ tests/Feature/MhsTest.php
@@ -24,17 +24,46 @@ class MhsTest extends TestCase
     public function test_create(){
         $response = $this->get('/mahasiswa');
 
-        $mhs = Mahasiswa::create([]); 
+        $mhs = Mahasiswa::create([
+        'Nim' => '2041720121',
+        'Nama' => 'Rizka M',
+        'Email'=> 'RizkaMusya@gmail.com',
+        'Alamat'=>'Tulungagung',
+        'Tanggal_Lahir'=>'2002-03-30',
+        'Jenis_Kelamin'=>'P',
+        'Jurusan'=>'Teknologi Informasi',
+        'Foto' =>'Foto testing',
+        ]); 
         $response->assertStatus(200);
+
+    }
+
+    public function test_read(){
         
-        
+        $response = $this->get('/mahasiswa');
+        $response->assertSee('Rizka M');
+        $response->assertStatus(200);
+
+
+    }
+
+    public function test_update(){
+
+        $mhs= Mahasiswa::where('Nama','Rizka M')-> update ([
+            'NIM'=>'2041720040',
+        ]);
+
+        $response = $this->get('/mahasiswa');
+        $response->assertSee(200);
 
     }
 
     public function test_delete(){
+        
+        $mhs= Mahasiswa::where('Nama','Rizka M')-> delete();
         $response = $this->get('/mahasiswa');
 
         $response->assertStatus(200);
-        $response->assertSee("Apakah Anda Yakin Menghapus Data Mahasiswa?");
+        $response->assertDontSee('NIM');
     }
 }
