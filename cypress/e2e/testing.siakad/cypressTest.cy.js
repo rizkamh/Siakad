300bdab53781abf494f18fc2d6228135c753ac15
rizka30 <2041720095@student.polinema.ac.id>
2022-09-29 22:40:08 +0700

    BlaxBox_Testing_FIX

--- cypress/e2e/testing.siakad/cypressTest.cy.js
+++ cypress/e2e/testing.siakad/cypressTest.cy.js
@@ -1,4 +1,4 @@
-describe('User Dapat Membuka Halaman Mahasiswa', () => {
+describe('Testing CRUD Untuk Membuka Halaman Mahasiswa', () => {
     it('Index Mahasiswa List', () => {
         cy.visit("http://127.0.0.1:8000/mahasiswa");
         cy.get('h2').should('have.text','JURUSAN TEKNOLOGI INFORMASI-POLITEKNIK NEGERI MALANG');
@@ -20,7 +20,7 @@ describe('User Dapat Membuka Halaman Mahasiswa', () => {
       cy.visit("http://127.0.0.1:8000/mahasiswa")
     })
 
-    //Fitur Show
+  //Fitur Show
     it('Show Mahasiswa List', () => {
         cy.visit("http://127.0.0.1:8000/mahasiswa");
         //show mahasiswa
@@ -31,28 +31,29 @@ describe('User Dapat Membuka Halaman Mahasiswa', () => {
         cy.get('.list-group > :nth-child(3)').contains('Email: aleshariza@gmail.com').and('be.visible');
         //button kembali
         cy.get('.btn').click();
-  })
+    })
 
   //Fitur Edit
-  it('Can Edit Data', () => {
-    cy.visit("http://127.0.0.1:8000/mahasiswa")
-    cy.get(':nth-child(10) > .btn-primary').click();
-    cy.get('#Nim').type("2041720011")
-    cy.get('#Nama').type("Rizka Hidayah")
-    cy.get('#Email').type("rizkamu@gmail.com")
-    cy.get('#Alamat').type("Tulungagung")
-    cy.get('#Tanggal_Lahir').type("2002-03-30")
-    cy.get('#Jurusan').type("Teknologi Informasi")
-    cy.get('#Jenis_Kelamin').type("P")
-    cy.get(':nth-child(11) > .form-control').selectFile('Rizkamu.png')
-    cy.get('.btn').contains("Submit").and("be.enabled");
-    cy.visit("http://127.0.0.1:8000/mahasiswa")
-  })
+    it('Can Edit Data', () => {
+      cy.visit("http://127.0.0.1:8000/mahasiswa")
+      cy.get(':nth-child(10) > .btn-primary').click();
+      cy.get('#Nim').type("2041720011")
+      cy.get('#Nama').type("Rizka Hidayah")
+      cy.get('#Email').type("rizkamu@gmail.com")
+      cy.get('#Alamat').type("Tulungagung")
+      cy.get('#Tanggal_Lahir').type("2002-03-30")
+      cy.get('#Jurusan').type("Teknologi Informasi")
+      cy.get('#Jenis_Kelamin').type("P")
+      cy.get(':nth-child(11) > .form-control').selectFile('Rizkamu.png')
+      cy.get('.btn').contains("Submit").and("be.enabled");
+      cy.visit("http://127.0.0.1:8000/mahasiswa")
+    })
 
   //Fitur Delete
-  it('Delete Mahasiswa List', () => {
-    cy.visit("http://127.0.0.1:8000/mahasiswa");
-    //delete mahasiswa
-    cy.get(':nth-child(10) > .btn-danger').click();
-  });
+    it('Delete Mahasiswa List', () => {
+      cy.visit("http://127.0.0.1:8000/mahasiswa");
+      //delete mahasiswa
+      cy.get(':nth-child(10) > .btn-danger').click();
+    });
+
 })
